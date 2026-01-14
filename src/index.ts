6ae275cbf71be64059c18cdf2daa8531147d9ddd
Michael Palmer <michael@solcast.com>
2026-01-15 09:11:52 +1100

    support snake case for response status

--- src/index.ts
+++ src/index.ts
@@ -1344,7 +1344,7 @@ export class JsonServiceClient {
 
         return this.json(res).then(o => {
             let errorDto = sanitize(o)
-            if (!errorDto.responseStatus)
+            if (!errorDto.responseStatus && !errorDto.response_status)
                 throw createErrorResponse(res.status, res.statusText, type)
             if (type != null)
                 errorDto.type = type
